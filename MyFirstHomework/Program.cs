5f68cea50047cd5451507e8bc6add336880ecde1
andry gamaliel <andrygonzalez077@gmail.com>
2024-10-03 20:26:40 -0400

    Class1
    Class1-

---
+++ MyFirstHomework/Program.cs
@@ -0,0 +1,56 @@
+﻿/*
+ I 
+ am 
+ developer 
+ for this 
+ ajajja: 
+*/
+
+Console.WriteLine("Hello, World!");
+
+// 1. declared and assigned variable
+
+int numberOne, numberTwo, result;
+
+numberOne = 1;
+numberTwo = 2;
+
+result= numberOne + numberTwo;
+
+Console.WriteLine(result);
+
+// 2. declared and assigned constand and change value
+
+const double pi = 3.14;
+
+Console.WriteLine(pi);
+Console.WriteLine(result*pi);
+
+// change value --- constant
+
+//pi = 1;
+
+// give error
+
+/*---------------------------------*/
+
+// 3. increment value
+
+numberOne = 2 + 2 + 5 + 6;
+
+Console.WriteLine(numberOne);
+
+// 4. declared floar
+
+float numberBigXD = 10152466.25f;
+
+byte result2 = (byte)(5 + numberBigXD);
+
+Console.WriteLine(result2);
+
+// 5. Date for the system (pc-now)
+
+DateTime dateNow = DateTime.Now;
+Console.WriteLine(dateNow);
+
+// end XD sjjsj
